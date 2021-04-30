38c79ade9fca7f235babd2b62adf3ab40463e449
Morgan Rae Saxbury Ward <mrsward@uwaterloo.ca>
2021-04-30 15:47:40 -0400

    newest pic overtakes image

--- components/Homepage.js
+++ components/Homepage.js
@@ -1,5 +1,5 @@
 import React, { Component, useState, useEffect} from 'react'
-import {View, Text, TouchableOpacity, Alert } from 'react-native'
+import {View, Text, TouchableOpacity, Alert, Image } from 'react-native'
 import { Camera } from 'expo-camera'
 import * as MediaLibrary from 'expo-media-library'
 import { homepageStyle } from './styleHome.js'
@@ -10,6 +10,7 @@ function Homepage (){
   const [hasPermission, setHasPermission] = useState(null);
   const [hasLibPermission, setHasLibPermission] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
+  const [photoUri, setPhotoUri] = useState("");
 
   useEffect(() => {
     (async () => {
@@ -32,8 +33,9 @@ function Homepage (){
 
   const takePictureAndSave = async () => {
       let photo = await camera.takePictureAsync()
-      console.log('uri', photo)
+      console.log(photo.uri)
       const asset = await MediaLibrary.saveToLibraryAsync(photo.uri)
+      setPhotoUri(photo.uri)
   };
 
   if (hasPermission === null ) {
@@ -45,27 +47,30 @@ function Homepage (){
 
   return (
   <View style={homepageStyle.container}>
-    <Camera style={homepageStyle.camera} type={type}  ref={(r) => {camera = r}}/>
-      <View style={homepageStyle.navBar}>
+    {
+      photoUri === ""
+      ? <Camera style={homepageStyle.camera} type={type}  ref={(r) => {camera = r}}/>
+      : <Image source={{uri: photoUri}} style={homepageStyle.camera} />
+    }
+    <View style={homepageStyle.navBar}>
+    <TouchableOpacity
+        style={homepageStyle.button}
+        onPress={() => takePictureAndSave()}>
+        <Ionicons name="caret-back-circle" size={32} color="black" />
+      </TouchableOpacity>
 
       <TouchableOpacity
-          style={homepageStyle.button}
-          onPress={() => takePictureAndSave()}>
-          <Ionicons name="caret-back-circle" size={32} color="black" />
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={homepageStyle.button}
-          onPress={() => {
-            setType(
-              type === Camera.Constants.Type.back
-                ? Camera.Constants.Type.front
-                : Camera.Constants.Type.back
-            );
-          }}>
-          <Ionicons name="sync-outline" size={32} color="black" />
-        </TouchableOpacity>
-      </View>
+        style={homepageStyle.button}
+        onPress={() => {
+          setType(
+            type === Camera.Constants.Type.back
+              ? Camera.Constants.Type.front
+              : Camera.Constants.Type.back
+          );
+        }}>
+        <Ionicons name="sync-outline" size={32} color="black" />
+      </TouchableOpacity>
+    </View>
   </View>
   );
 };
