664b68b0761c8520293cc92b91d00b98c5582753
Morgan Rae Saxbury Ward <mrsward@uwaterloo.ca>
2021-05-08 18:23:04 -0400

    OpenCV Installed for Windows

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -3,19 +3,22 @@ const axios = require('axios')
 
 const cv = require('opencv')
 
-cv.readImage('./hello.jpg', function (err, img) {
+cv.readImage("test.png", function(err, img) {
   if (err) {
     throw err;
   }
   console.log("The image is here morrrgan", img);
   const width = img.width();
+  console.log("image width is ", img.width())
   const height = img.height();
+  console.log("image height is ",img.height())
 
   if (width < 1 || height < 1) {
     throw new Error('Image has no size');
   }
 
   // do some cool stuff with img
+  
 
   // save img
   img.save('./img/myNewImage.jpg');
