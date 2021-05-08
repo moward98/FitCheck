930258e663d059ce23fcccdde2eb177c47a60dfb
rakif97 <rakif@edu.uwaterloo.ca>
2021-05-08 19:15:45 -0400

    image issue fixed

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,13 +1,12 @@
 const router = require('express').Router()
 const axios = require('axios')
-
 const cv = require('opencv')
 
-cv.readImage("test.png", function(err, img) {
+
+cv.readImage("./routes/fuck.jpg", function(err, img) {
   if (err) {
     throw err;
   }
-  console.log("The image is here morrrgan", img);
   const width = img.width();
   console.log("image width is ", img.width())
   const height = img.height();
@@ -17,11 +16,14 @@ cv.readImage("test.png", function(err, img) {
     throw new Error('Image has no size');
   }
 
-  // do some cool stuff with img
-  
-
-  // save img
-  img.save('./img/myNewImage.jpg');
+  img.detectObject(cv.FACE_CASCADE,{}, function(err, faces){
+    if (err) throw err;
+    for (var i = 0; i < faces.length; i++){
+       var face = faces[i];
+       img.ellipse(face.x + face.width / 2, face.y + face.height / 2,    face.width / 2, face.height / 2);
+     }
+       img.save("./routes/test2.jpg");
+     });
 });
 
 module.exports = router
